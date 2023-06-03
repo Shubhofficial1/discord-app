79efe88fefc1e7bd586d6d939d049c2cfa6c8130
shubham kumar <shubhofficial1@gmail.com>
2023-06-04 01:41:11 +0530

    Created Register functionality

--- discord-backend/controllers/authController.js
+++ discord-backend/controllers/authController.js
@@ -1,15 +1,57 @@
-// @desc    Auth user & get token
-// @route   POST /api/auth/login
-// @access  Public
-const loginUser = async (req, res) => {
-  res.send("Login Route...");
-};
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 // @desc    Register user & get token
 // @route   POST /api/auth/register
 // @access  Public
 const registerUser = async (req, res) => {
-  res.send("Register Route...");
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user already exists
+
+    const existsUser = await User.exists({ email: email.toLowerCase() });
+    // if yes , send a 409(conflict) response & respective message
+
+    if (existsUser) {
+      res.status(409).send("Email already in use.");
+    }
+
+    // if no , encrypt the password
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // create user document & save in database
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+    // create jwt token
+    const token = "JWT Token";
+
+    // send the response to client
+
+    if (user) {
+      res.status(201).json({
+        userDetails: {
+          email: user.email,
+          username: user.username,
+          token: token,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong , try again!");
+  }
+};
+
+// @desc    Auth user & get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = async (req, res) => {
+  res.send("Login Route...");
 };
 
 export { loginUser, registerUser };
---
+++ discord-backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
