0e9e5ce04172416e35ede05ac3f94f4f5582c32a
shubham kumar <shubhofficial1@gmail.com>
2023-06-03 17:40:12 +0530

    Initialized Project, install dependency & created server

---
+++ discord-backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT} in ${process.env.MODE} mode`);
+});
