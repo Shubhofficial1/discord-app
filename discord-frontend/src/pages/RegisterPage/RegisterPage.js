af94543dce5a39ef30ce67b722e2c1090b23cc5c
shubham kumar <shubhofficial1@gmail.com>
2023-06-04 18:09:02 +0530

    Added AuthBox Component

--- discord-frontend/src/pages/RegisterPage/RegisterPage.js
+++ discord-frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import AuthBox from "../../shared/components/AuthBox/AuthBox";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return <AuthBox>Register Page</AuthBox>;
 };
 
 export default RegisterPage;
